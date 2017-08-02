ab7a7f63802b03bec077537d51ddc6e1ae9b8b99
sandeshpatel <sandeshkrishna35@gamil.com>
2017-08-02 21:33:49 +0530

    added code in trim.py to read lines from log files which were addressed to whole class

---
+++ trim.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import re
+teacher = "sayan"
+# opening file to read from
+file_name = "Logs-2017-07-31-13-33.txt"
+log  = open(file_name, 'r')
+
+#regular expression to find lines which are addressed to whole class
+regular_expression = "^\[\d\d:\d\d\]\s<" + teacher + ">\s\S*[^:]\s"
+
+
+regex=re.compile(regular_expression) 
+
+#finding the lines which are matching the regular expression
+for line in log:
+	if re.match(regex, line ):
+		print(line)
